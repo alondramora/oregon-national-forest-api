a11515511d8d62fa79da6c861f7fb5c2845be460
alondramora <mora_ac@yahoo.com>
2022-11-17 13:32:04 -0800

    made sure port was set up dynamically

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const PORT = 8000
+const PORT = process.env.PORT ?? 8000
 
 app.use(cors())
 
@@ -115,6 +115,6 @@ app.get('/api/:name', (request, response) => {
     }
 })
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
     console.log(`The server is running on port ${PORT}`)
 })
